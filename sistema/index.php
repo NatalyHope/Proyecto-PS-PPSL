d6f403b79169dc9c9e01641d48cccc9f814a9e7c
jmscreep <116120961+jmscreep@users.noreply.github.com>
2023-09-13 21:23:28 -0600

    Add files via upload

---
+++ sistema/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+<?php include "scripts.php"?>
+	<title>Sistema Ventas</title>
+</head>
+<body>
+	<?php include "header.php"?>
+	<section id="container">
+		<center><h1>Bienvenido al sistema</h1></center>
+	</section>
+</body>
+</html>
